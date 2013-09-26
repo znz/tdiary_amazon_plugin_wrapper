2db6a9cfbd52ccea266d78c78ea0afc558a741c5
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2013-09-26 14:24:21 +0900

    add test

---
+++ spec/spec_helper.rb
@@ -0,0 +1,3 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'tdiary_amazon_plugin_wrapper'
---
+++ spec/tdiary_amazon_plugin_wrapper_spec.rb
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+
+describe TdiaryAmazonPluginWrapper do
+  it "without image" do
+    html = TdiaryAmazonPluginWrapper.new.isbn_image("4906470068")
+    html.must_match(%r!\A<a href="http://www\.amazon\.co\.jp/.*/dp/4906470068%3F.*"><img class="amazon" src="http://www\.tdiary\.org/images/amazondefaults/medium\.png"\nheight="160" width="122" alt="">\n.*</a>!)
+  end
+
+  it "with image" do
+    html = TdiaryAmazonPluginWrapper.new.isbn_image("4774158798")
+    html.must_match(%r!\A<a href="http://www\.amazon\.co\.jp/.*/dp/4774158798%3F.*"><img class="amazon" src="http://ecx\.images-amazon\.com/images/I/51K0jUf%2BiEL\._SL160_\.jpg"\nheight="160" width="126" alt="">\n.*</a>!)
+  end
+end
